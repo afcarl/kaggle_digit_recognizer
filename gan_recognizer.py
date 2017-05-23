dc4683081a11b51bcb4f374482752e79a3b46c22
felipe.ribeiro <felipe.ribeiro@smt.ufrj.br>
2017-05-23 15:59:19 -0300

    Added first models. Started main.

--- gan_recognizer.py
+++ gan_recognizer.py
@@ -6,6 +6,7 @@ Digit recognizer for Kaggle competition using GAN as classifier.
 
 import pandas as pd
 import numpy as np
+import argparse
 
 import torch # Torch variables handler
 import torch.nn as nn # Networks support
@@ -62,11 +63,121 @@ class KaggleMNIST(Dataset):
         label, img =  self.classes[index], self.imgs[index, :]
         
         # Reshape image
-        img = img.reshape((28, 28))
-        img = 
+        img = img.reshape((1, 28, 28))
         
         # Transforming img
         if self.transform is not None:
             img = self.transform(img)
+            
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+        
+        # Return
+        return img, target
+    
+    def __len__(self):
+        '''
+        Returns number of samples
+        @return Number of samples.
+        '''
+        return len(self.classes)
+
+class Gnet(nn.Module):
+    '''
+    Generator net.
+    '''
+    
+    def __init__(self):
+        '''
+        Initializes generator
+        '''
+        super(Gnet, self).__init__()
+        
+        # Network
+        self.layers = nn.Sequential(
+            # in: 100 x 1 x 1 out: 64 x 4 x 4
+            nn.Conv2d(100, 1024, 1, bias=False),
+            nn.PixelShuffle(4),
+            nn.InstanceNorm2d(64),
+            nn.ReLU(True),
+            # in: 64 x 4 x 4 out: 128 x 8 x 8
+            nn.Conv2d(64, 512, 1, bias=False),
+            nn.PixelShuffle(2),
+            nn.InstanceNorm2d(128),
+            nn.ReLU(True),
+            # in: 128 x 8 x 8 out: 128 x 16 x 16
+            nn.Conv2d(128, 512, 1, bias=False),
+            nn.PixelShuffle(2),
+            nn.InstanceNorm2d(128),
+            nn.ReLU(True),
+            # in: 128 x 16 x 16 out: 128 x 32 x 32
+            nn.Conv2d(128, 512, 1, bias=False),
+            nn.PixelShuffle(2),
+            nn.InstanceNorm2d(128),
+            nn.ReLU(True),
+            # in: 128 x 32 x32 out: 1 x 28 x 28
+            nn.Conv2d(128, 128, 5, bias=False),
+            nn.Conv2d(128, 1, 1, bias=False),
+            nn.Tanh()
+        )
+        
+    def forward(self, z):
+        '''
+        Forward pass
+        @param z input generator data.
+        @return generated image.
+        '''
+        return self.layers(z)
+    
+class Dnet(nn.Module):
+    '''
+    Discriminator net.
+    '''
+    
+    def __init__(self):
+        '''
+        Initializes generator
+        '''
+        super(Gnet, self).__init__()
+        
+        # Network
+        self.layers = nn.Sequential(
+            # in: 1 x 28 x 28 out: 64 x 14 x 14
+            nn.Conv2d(1, 64, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(0.2, inplace=True),
+            # in: 64 x 14 x 14 out: 128 x 7 x 7
+            nn.Conv2d(64, 128, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2, inplace=True),
+            # in: 128 x 7 x 7 out: 256 x 4 x 4
+            nn.Conv2d(128, 256, 4, bias=False),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2, inplace=True),
+            # in: 256 x 4 x 4 out: 128 x 2 x 2
+            nn.Conv2d(256, 512, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.ReLU(True),
+            # in: 512 x 2 x 2 out: 512 x 1 x 1
+            nn.Conv2d(512, 512, 4, 2, 1, bias=False),
+            nn.Conv2d(512, 10, 1, bias=False),
+            nn.LogSigmoid()
+        )
+        
+    def forward(self, x):
+        '''
+        Forward pass
+        @param x input image data.
+        @return classes log-probabilities.
+        '''
+        x = self.layers(x)
+        return x.view(x.size(0), -1)
+        
 
 if __name__ == '__main__':
+    '''
+    Main function.
+    '''
+    
+    # Parse
